8a94e507dbd2fbff6e3b27588274674980aa8be6
Francis Engels <tw3aky@gmail.com>
2025-04-24 23:54:45 +0200

    Setting up netrw

--- init.lua
+++ init.lua
@@ -7,6 +7,10 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- [[ Setting netrw options ]]
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = 30
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
